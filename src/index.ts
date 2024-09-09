1cfcb32c675332037b7f489c487c73b365673485
hemanttal <hemant.sachdeva@talentica.com>
2024-09-09 12:37:36 +0530

    more exports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,23 @@ import {analyzeFiles} from "./analysis/analyzer";
 import {FragmentState} from "./analysis/fragmentstate"
 import {AnalysisStateReporter} from "./output/analysisstatereporter";
 import {VisualizerGraphs, getVisualizerCallGraph} from "./output/visualizer";
+import {FunctionInfo, ModuleInfo, PackageInfo} from "./analysis/infos";
+import {locationToString} from "./misc/util";
+import {funcToStringWithCode} from "./output/tostringwithcode";
 
-export { Solver, expand, analyzeFiles, AnalysisStateReporter, getVisualizerCallGraph, VisualizerGraphs, FragmentState};
+export { Solver, 
+  expand, 
+  analyzeFiles, 
+  AnalysisStateReporter, 
+  getVisualizerCallGraph, 
+  VisualizerGraphs, 
+  FragmentState,
+  FunctionInfo,
+  ModuleInfo,
+  PackageInfo,
+  locationToString,
+  funcToStringWithCode
+};
 
 
 export function sayHello(name: string): string {
