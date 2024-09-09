8af8cc8424b00ee4e7b3856f11f95bee88105ffa
hemanttal <hemant.sachdeva@talentica.com>
2024-09-09 12:30:50 +0530

    export further

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import Solver from "./analysis/solver";
 import {expand} from "./misc/files";
 import {analyzeFiles} from "./analysis/analyzer";
+import {FragmentState} from "./analysis/fragmentstate"
 import {AnalysisStateReporter} from "./output/analysisstatereporter";
 import {VisualizerGraphs, getVisualizerCallGraph} from "./output/visualizer";
 
-export { Solver, expand, analyzeFiles, AnalysisStateReporter, getVisualizerCallGraph, VisualizerGraphs};
+export { Solver, expand, analyzeFiles, AnalysisStateReporter, getVisualizerCallGraph, VisualizerGraphs, FragmentState};
 
 
 export function sayHello(name: string): string {
